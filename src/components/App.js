7f9bd12fb404bda33016642c8c55f7b29bcfcd8d
Kuba Nieszporek <kubaz11011@gmail.com>
2020-02-09 18:52:56 +0100

    InputToDivAPI

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,47 @@ import Form from './Form';
 import Random from './Random';
 import './App.css';
 
+// api key
+const ApiKey = '43c0f116b91eccd6601d20c2b247d848'
+
 class App extends React.Component {
 
   state = {
     value: '',
     city: '',
     temp: '',
-    err: '',
+    country: '',
+    err: false,
   };
   
   handleCity = e => {
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=43c0f116b91eccd6601d20c2b247d848&units=metric`;
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${ApiKey}&units=metric`;
   
     fetch(API)
     .then(response => {
       if(response.ok){
+        console.clear();
         return response
       }
       throw Error("Nope")
     })
     .then(repsonse => repsonse.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
+    .then(data => {
+      this.setState({
+        err: false,
+        temp: data.main.temp,
+        city: this.state.value,
+        country: data.sys.country
+      })  
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState(state => ({
+        err: true,
+        city: this.state.value
+      }))
+  })
   }
-
   handleInputChange = (e) => {
     this.setState({
       value: e.target.value
@@ -38,7 +54,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Form value={this.state.value} change={this.handleInputChange} />
-        <Random />
+        <Random weather={this.state} />
       </div>
     );
   }
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const Random = () => {
+const Random = props => {
+
+    const {err, city, temp, country} = props.weather
+
+    let content = null;
+    if(!err && city){
+        content = (
+            <>
+            <div><em>{`CURRENTLY IN ${city}, ${country}, IT'S`}</em></div>
+            <div>{`${Math.round(temp)}°C`}</div>
+            </>
+        )
+    }
     return (
-        <button>RandomCity</button>
+        <>
+            <div className="result">
+                {err ? `Missing city ${city}` : content}
+            </div>
+        </>
     )
 }
 
